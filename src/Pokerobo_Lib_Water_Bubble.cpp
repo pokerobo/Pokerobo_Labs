e0b2587d443e78d0e3ef8d9423ee001aca71d394
pnhung177 <pnhung177@gmail.com>
2024-04-08 21:21:45 +0700

    Pokerobo_Lib_Water_Bubble - Add the isHit and explode methods

--- src/Pokerobo_Lib_Water_Bubble.cpp
+++ src/Pokerobo_Lib_Water_Bubble.cpp
@@ -17,22 +17,31 @@ int8_t Bubble::getSpeed() {
   return 2;
 }
 
+boolean Bubble::isHit(int8_t aimX, int8_t aimY) {
+  int16_t dx = abs(x-aimX);
+  int16_t dy = abs(y-aimY);
+  if (dx > r || dy > r) {
+    return false;
+  }
+  if (dx + dy <= r) {
+    return true;
+  }
+  if (sqrt(dx*dx + dy*dy)<= r) {
+    return true;
+  }
+  return false;
+}
+
+void Bubble::explode() {
+  this->y = -1 - this->r;
+}
+
 Aquarium::Aquarium(void* u8g2Ref, lcd_layout_t layout, uint8_t total) {
   _total = (total <= WATER_BUBBLES_TOTAL) ? total : WATER_BUBBLES_TOTAL;
   _u8g2Ref = u8g2Ref;
   _layout = layout;
-  switch (_layout) {
-    case LCD_LAYOUT_R0:
-    case LCD_LAYOUT_R2:
-      _maxX = 127;
-      _maxY = 63;
-      break;
-    case LCD_LAYOUT_R1:
-    case LCD_LAYOUT_R3:
-      _maxX = 63;
-      _maxY = 127;
-      break;
-  }
+  _maxX = (_layout == LCD_LAYOUT_R0 || _layout == LCD_LAYOUT_R2) ? 127 : 63;
+  _maxY = (_layout == LCD_LAYOUT_R0 || _layout == LCD_LAYOUT_R2) ? 63 : 127;
 }
 
 void Aquarium::begin() {
@@ -64,4 +73,16 @@ void Aquarium::render() {
     Bubble *b = &_bubbles[i];
     u8g2->drawCircle(b->x, b->y, b->r);
   }
+}
+
+int8_t Aquarium::shoot(int8_t aimX, int8_t aimY) {
+  int8_t count = 0;
+  for (uint8_t i=0; i<_total; i++) {
+    Bubble *b = &_bubbles[i];
+    if (b->isHit(aimX, aimY)) {
+      b->explode();
+      count++;
+    }
+  }
+  return count;
 }
--- src/Pokerobo_Lib_Water_Bubble.h
+++ src/Pokerobo_Lib_Water_Bubble.h
@@ -8,13 +8,17 @@
 #endif//WATER_BUBBLES_TOTAL
 
 class Bubble {
+  friend class Aquarium;
   public:
     Bubble();
-    int16_t x;
-    int16_t y;
-    int8_t r;
     bool isDisappeared();
     int8_t getSpeed();
+    boolean isHit(int8_t aimX, int8_t aimY);
+    void explode();
+  private:
+    int8_t r;
+    int16_t x;
+    int16_t y;
 };
 
 class Aquarium {
@@ -23,6 +27,7 @@ class Aquarium {
     void begin();
     void change();
     void render();
+    int8_t shoot(int8_t x, int8_t y);
   private:
     uint8_t _total = 0;
     Bubble _bubbles[WATER_BUBBLES_TOTAL];

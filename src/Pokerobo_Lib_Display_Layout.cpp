4e1e1346a3e5e695edebb3592ea44199f9ce8c6f
pnhung177 <pnhung177@gmail.com>
2024-04-11 11:36:12 +0700

    Pokerobo_Lib_Display_Layout - add the CoordinateAxes class

--- src/Pokerobo_Lib_Display_Layout.cpp
+++ src/Pokerobo_Lib_Display_Layout.cpp
@@ -1,3 +1,29 @@
 #include <U8g2lib.h>
 #include "Pokerobo_Lib_Display_Layout.h"
 
+CoordinateAxes::CoordinateAxes(void* u8g2Ref, lcd_layout_t layout) {
+  _u8g2Ref = u8g2Ref;
+  setLcdLayout(layout);
+}
+
+void CoordinateAxes::setLcdLayout(lcd_layout_t layout) {
+  _layout = layout;
+  _maxX = (_layout == LCD_LAYOUT_R0 || _layout == LCD_LAYOUT_R2) ? 127 : 63;
+  _maxY = (_layout == LCD_LAYOUT_R0 || _layout == LCD_LAYOUT_R2) ? 63 : 127;
+}
+
+lcd_layout_t CoordinateAxes::getLcdLayout() {
+  return _layout;
+}
+
+void* CoordinateAxes::getU8g2Ref() {
+  return _u8g2Ref;
+}
+
+int8_t CoordinateAxes::getMaxX() {
+  return _maxX;
+}
+
+int8_t CoordinateAxes::getMaxY() {
+  return _maxY;
+}
--- src/Pokerobo_Lib_Display_Layout.h
+++ src/Pokerobo_Lib_Display_Layout.h
@@ -12,8 +12,17 @@ typedef enum {
 
 class CoordinateAxes {
   public:
-    CoordinateAxes(void* u8g2Ref, lcd_layout_t layout);
-    void render();
+    CoordinateAxes(void* u8g2Ref, lcd_layout_t layout=LCD_LAYOUT_R0);
+    virtual void setLcdLayout(lcd_layout_t layout);
+    virtual lcd_layout_t getLcdLayout();
+    virtual void* getU8g2Ref();
+    virtual int8_t getMaxX();
+    virtual int8_t getMaxY();
+  private:
+    void* _u8g2Ref;
+    lcd_layout_t _layout;
+    int8_t _maxX;
+    int8_t _maxY;
 };
 
 #endif

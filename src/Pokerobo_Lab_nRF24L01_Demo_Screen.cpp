1532341b4da1b55fd70b933f4a109f94c5ca3c00
pnhung177 <pnhung177@gmail.com>
2024-04-23 11:04:08 +0700

    Pokerobo_Lab_nRF24L01_Demo_Screen - refactor source code

--- src/Pokerobo_Lab_nRF24L01_Demo_Screen.cpp
+++ src/Pokerobo_Lab_nRF24L01_Demo_Screen.cpp
@@ -1,1 +1,18 @@
 #include "Pokerobo_Lab_nRF24L01_Demo_Screen.h"
+
+SimpleDisplayHandler::SimpleDisplayHandler(char *title): DisplayHandler() {
+  _title = title;
+}
+void SimpleDisplayHandler::renderMessage(char *text) {
+  U8G2 *_u8g2 = (U8G2*)_u8g2Ref;
+  int8_t _charHeight = _u8g2->getMaxCharHeight();
+  int8_t _charWidth = _u8g2->getMaxCharWidth();
+  this->firstPage();
+  do {
+    _u8g2->drawFrame(1, _charHeight + 1, 128 - 2, 64 - _charHeight - 2);
+    if (_title != NULL) {
+      _u8g2->drawStr(64 - _charWidth * strlen(_title) / 2, _charHeight, _title);
+    }
+    _u8g2->drawStr(64 - _charWidth * strlen(text) / 2, 32 + _charHeight / 2, text);
+  } while (this->nextPage());
+}
--- src/Pokerobo_Lab_nRF24L01_Demo_Screen.h
+++ src/Pokerobo_Lab_nRF24L01_Demo_Screen.h
@@ -7,22 +7,8 @@
 class SimpleDisplayHandler: public DisplayHandler {
   public:
     using DisplayHandler::DisplayHandler;
-    SimpleDisplayHandler(char *title): DisplayHandler() {
-      _title = title;
-    }
-    void renderMessage(char *text) {
-      U8G2 *_u8g2 = (U8G2*)_u8g2Ref;
-      int8_t _charHeight = _u8g2->getMaxCharHeight();
-      int8_t _charWidth = _u8g2->getMaxCharWidth();
-      this->firstPage();
-      do {
-        _u8g2->drawFrame(1, _charHeight + 1, 128 - 2, 64 - _charHeight - 2);
-        if (_title != NULL) {
-          _u8g2->drawStr(64 - _charWidth * strlen(_title) / 2, _charHeight, _title);
-        }
-        _u8g2->drawStr(64 - _charWidth * strlen(text) / 2, 32 + _charHeight / 2, text);
-      } while (this->nextPage());
-    }
+    SimpleDisplayHandler(char *title);
+    void renderMessage(char *text);
   private:
     char *_title = NULL;
 };

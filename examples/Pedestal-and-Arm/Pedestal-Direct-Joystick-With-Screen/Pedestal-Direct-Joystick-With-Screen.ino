3633c6eac52c949569237dd5ef4f02f995c02ac3
pnhung177 <pnhung177@gmail.com>
2024-05-04 09:12:03 +0700

    Add the Pedestal-and-Arm/Pedestal-Direct-Joystick-With-Screen example

---
+++ examples/Pedestal-and-Arm/Pedestal-Direct-Joystick-With-Screen/Pedestal-Direct-Joystick-With-Screen.ino
@@ -0,0 +1,37 @@
+#include <Pokerobo_RCB_master.h>
+#include "Pokerobo_Lab_Pedestal_Controller.h"
+#include "Pokerobo_Lab_Pedestal_Screen.h"
+
+JoystickHandler joystickHandler;
+JoystickAction action;
+
+PedestalDisplayHandler displayHandler;
+
+PedestalHandler pedestalHandler1(4, 5);
+PedestalHandler pedestalHandler3(8, 9);
+
+PedestalHandler* pedestalHandlers[PEDESTALS_MAX] = {
+  &pedestalHandler1,
+  &pedestalHandler3,
+};
+
+PedestalGroup pedestalGroup(pedestalHandlers);
+PedestalController pedestalController(&pedestalGroup);
+
+void setup() {
+  Serial.begin(57600);
+
+  displayHandler.begin();
+  joystickHandler.begin();
+
+  pedestalGroup.begin();
+  pedestalController.begin();
+}
+
+void loop() {
+  joystickHandler.input(&action);
+  pedestalController.play(action.getPressingFlags(), action.getX(), action.getY());
+  displayHandler.render(&action);
+
+  delay(50);
+}

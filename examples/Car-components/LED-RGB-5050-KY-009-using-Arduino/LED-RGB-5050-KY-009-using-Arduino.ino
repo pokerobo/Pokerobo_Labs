b826065898633b6227d842a73c02e4364ed31094
pnhung177 <pnhung177@gmail.com>
2024-05-11 10:22:57 +0700

    Add the RGB-LED-KY-009 and Traffic-light examples

---
+++ examples/Car-components/LED-RGB-5050-KY-009-using-Arduino/LED-RGB-5050-KY-009-using-Arduino.ino
@@ -0,0 +1,26 @@
+int R_PIN =  9; // select the pin for the red LED
+int G_PIN = 10; // select the pin for the green LED
+int B_PIN = 11; // select the pin for the  blue LED
+
+void setup() {
+  pinMode(R_PIN, OUTPUT);
+  pinMode(G_PIN, OUTPUT);
+  pinMode(B_PIN, OUTPUT);
+  Serial.begin(57600);
+}
+
+void loop() {
+  int val;
+  for(val = 255; val > 0; val--) {
+    analogWrite(R_PIN, val);
+    analogWrite(G_PIN, 128 - val);
+    analogWrite(B_PIN, 255 - val);
+    delay(1); 
+  }
+  for(val = 0; val < 255; val++) {
+    analogWrite(R_PIN, val);
+    analogWrite(G_PIN, 128 - val);
+    analogWrite(B_PIN, 255 - val);
+    delay(1);
+  }
+}
---
+++ examples/Car-components/Traffic-Light-using-Arduino/Traffic-Light-using-Arduino.ino
@@ -0,0 +1,35 @@
+int R_LED = 13; //Red light
+int Y_LED = 12; //Yellow light
+int G_LED = 11; //Green light
+
+void setup() {
+  pinMode(R_LED, OUTPUT);
+  pinMode(Y_LED, OUTPUT);
+  pinMode(G_LED, OUTPUT);
+}
+
+void loop() {
+  // Green light: go
+  digitalWrite(R_LED, LOW);
+  digitalWrite(Y_LED, LOW);
+  digitalWrite(G_LED, HIGH);
+  delay(30000); //wait 30 seocnds
+
+  // Yellow light: red light is coming
+  digitalWrite(R_LED, LOW);
+  digitalWrite(Y_LED, HIGH);
+  digitalWrite(G_LED, LOW);
+  delay(2000); //wait 2 seocnds
+
+  // Red light: stop
+  digitalWrite(R_LED, HIGH);
+  digitalWrite(Y_LED, LOW);
+  digitalWrite(G_LED, LOW);
+  delay(30000); //wait 30 seocnds
+
+  // Green light is coming
+  digitalWrite(R_LED, HIGH);
+  digitalWrite(Y_LED, HIGH);
+  digitalWrite(G_LED, LOW);
+  delay(2000); //wait 2 seocnds
+}

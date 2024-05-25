346830d9e34974af4ba09d9fba3082ee07e02d72
pnhung177 <pnhung177@gmail.com>
2024-05-25 11:43:46 +0700

    Update steps in the Motor-L298N-RoboCarHandler-move-A1 example

--- examples/Car-Components/Motor-L298N-RoboCarHandler-move-A1/Motor-L298N-RoboCarHandler-move-A1.ino
+++ examples/Car-Components/Motor-L298N-RoboCarHandler-move-A1/Motor-L298N-RoboCarHandler-move-A1.ino
@@ -15,18 +15,18 @@ void setup() {
   roboCarHandler.move(1, 255, 255, 1);
   delay(1000);
   // turn forward on right for 100 milliseconds
-  roboCarHandler.move(1, 127, 127, 0);
-  delay(100);
-  // Moving forward for 1s
-  roboCarHandler.move(1, 255, 255, 1);
-  delay(1000);
-
+  roboCarHandler.move(1, 255, 255, 0);
+  delay(150);
   // Moving backward for 1s
   roboCarHandler.move(-1, 255, 255, -1);
   delay(1000);
+
+  // Moving forward for 1s
+  roboCarHandler.move(1, 255, 255, 1);
+  delay(1000);
   // turn backward on left for 100 milliseconds
-  roboCarHandler.move(0, 127, 127, -1);
-  delay(100);
+  roboCarHandler.move(-1, 255, 255, 0);
+  delay(150);
   // Moving backward for 1s
   roboCarHandler.move(-1, 255, 255, -1);
   delay(1000);

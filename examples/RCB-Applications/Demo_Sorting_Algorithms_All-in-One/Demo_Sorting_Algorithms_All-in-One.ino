a4ec054c07757985a1823515c3511daa268f1af9
pnhung177 <pnhung177@gmail.com>
2025-03-12 20:27:45 +0700

    feat/SortingRunner: add the all-in-one example

---
+++ examples/RCB-Applications/Demo_Sorting_Algorithms_All-in-One/Demo_Sorting_Algorithms_All-in-One.ino
@@ -0,0 +1,30 @@
+#include "Pokerobo_Lab_SortingRunners.h"
+
+JoystickHandler joystickHandler;
+GeometryDisplayHandler displayHandler;
+
+SampleManager sample(&displayHandler);
+
+ProgramSelector programSelector(&displayHandler, &joystickHandler, SCREEN_FLOW_CONFIGURATION);
+
+void setup() {
+  Serial.begin(57600);
+
+  joystickHandler.begin();
+  displayHandler.begin();
+
+  sample.begin();
+
+  programSelector.add(new BubbleSortingRunner(&sample));
+  programSelector.add(new InsertionSortingRunner(&sample));
+  programSelector.add(new SelectionSortingRunner(&sample));
+  programSelector.add(new MergeSortingRunner(&sample));
+  programSelector.add(new QuickSortingRunner(&sample));
+  // programSelector.add(new HeapSortingRunner(&sample));
+
+  programSelector.begin();
+}
+
+void loop() {
+  programSelector.check();
+}

ef8e19484fddbc2b7b9ee4df6317901bf0d6254f
pnhung177 <pnhung177@gmail.com>
2025-03-20 09:43:06 +0700

    Renames the LCD12864_I2C_U8G2_Example_Lines_Bresenham examples

--- examples/RCB-LCD-Display/LCD12864_I2C_U8G2_Example_Lines_Bresenham-1/LCD12864_I2C_U8G2_Example_Lines_Bresenham-1.ino
+++ examples/RCB-LCD-Display/LCD12864_I2C_U8G2_Example_Lines_Bresenham-1/LCD12864_I2C_U8G2_Example_Lines_Bresenham-1.ino
@@ -7,7 +7,7 @@ void drawLine(int x0, int y0, int x1, int y1) {
   int dy = abs(y1-y0), sy = y0<y1 ? 1 : -1; 
   int err = (dx>dy ? dx : -dy)/2, e2;
 
-  for(;;){
+  for(;;) {
     displayHandler.drawPixel(x0, y0);
     if (x0==x1 && y0==y1) break;
     e2 = err;

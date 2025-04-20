fb5c90c1914c1833938f469072d7bf26d75078bc
pnhung177 <pnhung177@gmail.com>
2025-04-20 19:00:45 +0700

    feat/arduino-ble: add the Orientation-Tracking/GY-521-Jump-Detecting-Peripheral example

---
+++ examples/Orientation-Tracking/GY-521-Jump-Detecting-Peripheral/GY-521-Jump-Detecting-Peripheral.ino
@@ -0,0 +1,38 @@
+#include <Wire.h>
+#include <MPU6050.h>
+#include "Pokerobo_Lab_BLE_Peripheral.h"
+
+MPU6050 mpu;
+
+int16_t ax, ay, az;  // Gia tốc trên các trục
+
+class MyBLEGenerator: public PokeroboBLEGenerator {
+  public:
+    bool generate(int32_t &value) {
+      mpu.getAcceleration(&ax, &ay, &az);
+      value = az;
+      return true;
+    }
+};
+
+MyBLEGenerator counter;
+PokeroboBLEPeripheralDebugLogger serialLogger;
+PokeroboBLEPeripheral counterPeripheral("2A56", "180C", &counter, &serialLogger);
+
+void setup() {
+  Wire.begin();
+  Serial.begin(57600);
+
+  mpu.initialize();
+
+  if (!mpu.testConnection()) {
+    Serial.println("MPU6500 is not connected!");
+    while (1);
+  }
+
+  counterPeripheral.begin("UNO-R4-BLE");
+}
+
+void loop() {
+  counterPeripheral.check();
+}
